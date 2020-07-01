243154c065a83a097b374be375da45b769e7f605
Stephen Kelly <stevekm@users.noreply.github.com>
2020-07-01 09:48:53 -0400

    add test suite script

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Run all the unit tests
+"""
+
+import unittest
+import sys
+
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    start_dir = '.'
+    suite = loader.discover(start_dir)
+
+    runner = unittest.TextTestRunner()
+    ret = not runner.run(suite).wasSuccessful()
+    sys.exit(ret)
