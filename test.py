9cf21aafe49236d762995b529b6e531546283425
Stephen Kelly <stevekm@users.noreply.github.com>
2021-01-22 16:57:51 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ Run all the unit tests
 
 NOTE: This does not seem to work correctly, it skips some tests located in the `tests/` directory, 
 so do not use this and instead use the `make test` recipes from the Makefile
+TODO: figure out what is wrong with this script
 """
 
 import unittest
