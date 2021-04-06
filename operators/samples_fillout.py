8387e6746d545f26c3e36822f2dd15326f169b5e
Stephen Kelly <stevekm@users.noreply.github.com>
2021-04-06 18:15:43 -0400

    fix samples fillout operator test cases and operator to use updated input schema

--- operators/samples_fillout.py
+++ operators/samples_fillout.py
@@ -13,20 +13,21 @@ class SamplesFillout(Operator):
     cwl_file = CWLFile('samples_fillout_workflow.cwl')
     def __init__(self, samplesheet, **kwargs):
         super().__init__(**kwargs)
+        self.args['bam_files'] = []
+        self.args['maf_files'] = []
+        self.args['sample_ids'] = []
         samplesheet = os.path.abspath(samplesheet)
+        # load the records from the samplesheet
         with open(samplesheet) as f:
             reader = csv.DictReader(f, delimiter = '\t')
-            rows = [ row for row in reader ]
+            for row in reader:
+                self.args['bam_files'].append(row['bam_file'])
+                self.args['maf_files'].append(row['maf_file'])
+                self.args['sample_ids'].append(row['sample_id'])
 
+        # convert all inputs to CWL input format
         self.input = generate_input(
             self.args,
-            File_keys = ['ref_fasta']
+            File_keys = ['ref_fasta'],
+            list_File_keys = ['bam_files', 'maf_files']
         )
-        self.input['samples'] = []
-        for row in rows:
-            sample = {
-                'sample_id': row['sample_id'],
-                'maf_file': {'class': 'File', 'path': row['maf_file']},
-                'bam_file': {'class': 'File', 'path': row['bam_file']}
-            }
-            self.input['samples'].append(sample)
--- tests/test_samples_fillout_workflow_operator.py
+++ tests/test_samples_fillout_workflow_operator.py
@@ -15,42 +15,77 @@ from operators.samples_fillout import SamplesFillout
 sys.path.pop(0)
 
 class TestSamplesFilloutWorkflowOperator(PlutoTestCase):
+    def setUp(self):
+        super().setUp()
+        # make a demo samplesheet from the demo files
+        self.maf1 = os.path.join(self.DATA_SETS['demo']['MAF_DIR'], "Sample1.Sample2.muts.maf")
+        self.maf2 = os.path.join(self.DATA_SETS['demo']['MAF_DIR'], "Sample4.Sample3.muts.maf")
+        self.bam1 = os.path.join(self.DATA_SETS['demo']['BAM_DIR'], "Sample1.bam")
+        self.bam2 = os.path.join(self.DATA_SETS['demo']['BAM_DIR'], "Sample4.bam")
+        self.ref_fasta = self.DATA_SETS['demo']['REF_FASTA']
+        lines = [
+        ['sample_id', 'bam_file', 'maf_file'],
+        ['Sample1', self.bam1, self.maf1],
+        ['Sample4', self.bam2, self.maf2],
+        ]
+        self.samplesheet = self.write_table(self.tmpdir, filename = "samples.tsv", lines = lines)
+
     def test_operator_inputs(self):
         """
         Test that the CWL inputs are created correctly based on Operator args
         """
         self.maxDiff = None
-        maf1 = os.path.join(self.DATA_SETS['demo']['MAF_DIR'], "Sample1.maf")
-        maf2 = os.path.join(self.DATA_SETS['demo']['MAF_DIR'], "Sample2.maf")
-        bam1 = os.path.join(self.DATA_SETS['demo']['BAM_DIR'], "Sample1.bam")
-        bam2 = os.path.join(self.DATA_SETS['demo']['BAM_DIR'], "Sample2.bam")
-        ref_fasta = self.DATA_SETS['demo']['REF_FASTA']
-        lines = [
-        ['sample_id', 'bam_file', 'maf_file'],
-        ['Sample1', bam1, maf1],
-        ['Sample2', bam2, maf2],
-        ]
-        samplesheet = self.write_table(self.tmpdir, filename = "samples.tsv", lines = lines)
-
-        operator = SamplesFillout(samplesheet = samplesheet, ref_fasta = ref_fasta)
+        operator = SamplesFillout(samplesheet = self.samplesheet, ref_fasta = self.ref_fasta)
         input = operator.input
-        # print(input)
+
+        # this is the old input format that works with CWL version 1.1
+        # expected_input = {
+        #     'ref_fasta': {'class': 'File', 'path': ref_fasta},
+        #     'samples': [
+        #         {
+        #             'sample_id': 'Sample1',
+        #             'maf_file': {'class': 'File', 'path': maf1},
+        #             'bam_file': {'class': 'File', 'path': bam1}
+        #             },
+        #         {
+        #             'sample_id': 'Sample2',
+        #             'maf_file': {'class': 'File', 'path': maf2},
+        #             'bam_file': {'class': 'File', 'path': bam2}
+        #         }
+        #     ]
+        # }
         expected_input = {
-            'ref_fasta': {'class': 'File', 'path': ref_fasta},
-            'samples': [
-                {
-                    'sample_id': 'Sample1',
-                    'maf_file': {'class': 'File', 'path': maf1},
-                    'bam_file': {'class': 'File', 'path': bam1}
-                    },
-                {
-                    'sample_id': 'Sample2',
-                    'maf_file': {'class': 'File', 'path': maf2},
-                    'bam_file': {'class': 'File', 'path': bam2}
-                }
-            ]
+        'sample_ids': ['Sample1', 'Sample4'],
+        'bam_files': [{'class': 'File', 'path': self.bam1}, {'class': 'File', 'path': self.bam2}],
+        'maf_files': [{'class': 'File', 'path': self.maf1}, {'class': 'File', 'path': self.maf2}],
+        'ref_fasta': {'class': 'File', 'path': self.ref_fasta}
         }
         self.assertEqual(input, expected_input)
+
+    def test_operator_runs1(self):
+        """
+        Test case that the Operator can run the CWL correctly
+        """
+        operator = SamplesFillout(samplesheet = self.samplesheet, ref_fasta = self.ref_fasta,
+            dir=self.tmpdir, output_dir=self.tmpdir, verbose = False)
+        output_json, output_dir, output_json_file = operator.run()
+        expected_output = {
+            'output_file':
+                {
+                'location': 'file://' + os.path.join(output_dir, 'output.maf'),
+                'basename': 'output.maf',
+                'class': 'File',
+                'checksum': 'sha1$3fec625cac6d500a0bc074753ce686a5685229f4',
+                'size': 17724,
+                'path': os.path.join(output_dir, 'output.maf')
+                }
+            }
+        self.maxDiff = None
+        self.assertDictEqual(output_json, expected_output)
+
+        path = output_json['output_file']['path']
+        comments, mutations = self.load_mutations(path)
+        self.assertEqual(len(mutations), 106)
 
 
 
