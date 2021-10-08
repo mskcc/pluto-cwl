bce8a0e8ad2756a6081f45ccc6db31324e18bf92
Stephen Kelly <stevekm@users.noreply.github.com>
2021-10-08 08:49:10 -0400

    update env and pip requirements

--- env.juno.sh
+++ env.juno.sh
@@ -8,7 +8,7 @@ arg="${1:-None}"
 case $arg in
     # for running test cases
     test)
-        module load singularity/3.3.0
+        module load singularity/3.7.1
         module load python/3.7.1
         module load cwl/cwltool
         ;;
@@ -16,7 +16,7 @@ case $arg in
     # for running all test cases including the large integration tests
     test-full)
         export LARGE_TESTS=True
-        module load singularity/3.3.0
+        module load singularity/3.7.1
         module load python/3.7.1
         module load cwl/cwltool
         ;;
@@ -24,12 +24,12 @@ case $arg in
     # for running CWL's with Singularity
     singularity)
         unset SINGULARITY_CACHEDIR  # TODO: why do I need to unset this when I am explicitly setting it in the Makefile??
-        module load singularity/3.3.0
+        module load singularity/3.7.1
         ;;
 
     # for interactive shell session
     shell)
-        module load singularity/3.3.0
+        module load singularity/3.7.1
         module load python/3.7.1
         module load cwl/cwltool
         module load jq
@@ -44,15 +44,16 @@ case $arg in
 
     # for using conda installed Toil and Singularity to run CWL's
     toil)
-        # need to get these env vars to propagate into the HPC jobs
-        # SINGULARITY_DOCKER_USERNAME
-        # SINGULARITY_DOCKER_PASSWORD
-        [ -e ../toil-settings.sh ] && . ../toil-settings.sh || :
+        # need to get these env vars to propagate into the HPC jobs; SINGULARITY_DOCKER_USERNAME  SINGULARITY_DOCKER_PASSWORD
+        [ -e /juno/work/ci/kellys5/projects/toil-settings/toil-settings.sh ] && \
+        . /juno/work/ci/kellys5/projects/toil-settings/toil-settings.sh || \
+        echo ">>> WARNING: could not find file toil-settings.sh, needed for SINGULARITY_DOCKER_USERNAME and SINGULARITY_DOCKER_PASSWORD; HPC jobs might break!"
+
         # NOTE: in the future might also need these
         # TOIL_LSF_ARGS
         # SINGULARITY_PULLDIR
         # SINGULARITY_CACHEDIR
-        module load singularity/3.3.0
+        module load singularity/3.7.1
         export PATH=${PWD}/conda/bin:${PATH}
         export PATH="$(dirname $(which singularity))":${PATH}
         unset PYTHONPATH
