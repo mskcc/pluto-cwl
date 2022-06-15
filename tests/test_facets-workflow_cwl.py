7c69c5d5eca26c9d0e6add7faaa2b36448ea6158
Stephen Kelly <stevekm@users.noreply.github.com>
2022-06-15 11:46:49 -0400

    clean up tests/test_facets-workflow_cwl.py

--- tests/test_facets-workflow_cwl.py
+++ tests/test_facets-workflow_cwl.py
@@ -43,160 +43,55 @@ class TestFacetsWorkflow(PlutoTestCase):
         }
 
         output_json, output_dir = self.run_cwl()
+        output_dir_sample24 = os.path.join(output_dir, 'facets/Sample24.Sample23')
 
         self.maxDiff = None
         expected_output = {
         'annotated_maf': [
-        OFile(name = "Sample24.Sample23_hisens.ccf.portal.maf", hash = "d91a8e15c66429b09f1b7db41bc38bdfa0b84c64", size = 11996607, dir = os.path.join(output_dir, 'facets/Sample24.Sample23'))
-        ],
+            OFile(name = "Sample24.Sample23_hisens.ccf.portal.maf", hash = "d91a8e15c66429b09f1b7db41bc38bdfa0b84c64", size = 11996607, dir = output_dir_sample24)
+            ],
         'arm_level_txt': [
-        OFile(name = "Sample24.arm_level.txt", hash = "df37c54ae4969257e436a7a7a595c42ef19ecbb5", size = 1824, dir = os.path.join(output_dir, 'facets/Sample24.Sample23'))
-        ],
+            OFile(name = "Sample24.arm_level.txt", hash = "df37c54ae4969257e436a7a7a595c42ef19ecbb5", size = 1824, dir = output_dir_sample24)
+            ],
         'facets_txt': [
-        OFile(name = "Sample24.txt", hash = "a0fb3df832efc18a66a8a54e5609666da5f4d7d7", size = 529, dir = os.path.join(output_dir, 'facets/Sample24.Sample23'))
-        ],
+            OFile(name = "Sample24.txt", hash = "a0fb3df832efc18a66a8a54e5609666da5f4d7d7", size = 529, dir = output_dir_sample24)
+            ],
         'failed_pairs': [],
         'gene_level_txt': [
-        OFile(name = "Sample24.gene_level.txt", hash = "4e916a52458151007486bf536acfff539fdc2ecc", size = 148195, dir = os.path.join(output_dir, 'facets/Sample24.Sample23'))
-        ],
-        'hisens_rds': [
-        OFile(name = "Sample24_hisens.rds", hash = "6bfd6c7f29c49ec8ef538dd468a3b4626b05bda2", size = 213986, dir = os.path.join(output_dir, 'facets/Sample24.Sample23'))
-        ],
-        'output_dir': {
-            'basename': 'facets',
-            'class': 'Directory',
-            'listing': [
-                {
-                    'basename': 'Sample24.Sample23',
-                    'class': 'Directory',
-                    'listing': [
-                        {
-                            'basename': 'Sample24.Sample23_hisens.ccf.portal.maf',
-                            'checksum': 'sha1$d91a8e15c66429b09f1b7db41bc38bdfa0b84c64',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.Sample23_hisens.ccf.portal.maf'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.Sample23_hisens.ccf.portal.maf'),
-                            'size': 11996607
-                        },
-                        {
-                            'basename': 'Sample24.arm_level.txt',
-                            'checksum': 'sha1$df37c54ae4969257e436a7a7a595c42ef19ecbb5',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.arm_level.txt'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.arm_level.txt'),
-                            'size': 1824
-                        },
-                        {
-                            'basename': 'Sample24.txt',
-                            'checksum': 'sha1$a0fb3df832efc18a66a8a54e5609666da5f4d7d7',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.txt'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.txt'),
-                            'size': 529
-                        },
-                        {
-                            'basename': 'Sample24.gene_level.txt',
-                            'checksum': 'sha1$4e916a52458151007486bf536acfff539fdc2ecc',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.gene_level.txt'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.gene_level.txt'),
-                            'size': 148195
-                        },
-                        {
-                            'basename': 'Sample24_hisens.cncf.txt',
-                            'checksum': 'sha1$db9131a33889a1cac82e3bd6b3f0e5e182c65105',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.cncf.txt'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.cncf.txt'),
-                            'size': 5238
-                        },
-                        {
-                            'basename': 'Sample24_hisens.rds',
-                            'checksum': 'sha1$6bfd6c7f29c49ec8ef538dd468a3b4626b05bda2',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.rds'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.rds'),
-                            'size': 213986
-                        },
-                        {
-                            'basename': 'Sample24_hisens.seg',
-                            'checksum': 'sha1$652f9c6d0245af49bac6ca67a089af7d4e46801b',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.seg'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.seg'),
-                            'size': 1897
-                        },
-                        {
-                            'basename': 'Sample24_purity.rds',
-                            'checksum': 'sha1$dd8b967f84b191ff76214c6110db8d0e65f6514c',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_purity.rds'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_purity.rds'),
-                            'size': 213356
-                        },
-                        {
-                            'basename': 'Sample24_purity.seg',
-                            'checksum': 'sha1$591e6d8b432e1e910fe4fb4b1814508131f960c9',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_purity.seg'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_purity.seg'),
-                            'size': 1285
-                        },
-                        {
-                            'basename': 'Sample24.qc.txt',
-                            'checksum': 'sha1$d4a36726a5fcb7b268aae02d97ce4e382e42d9f6',
-                            'class': 'File',
-                            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.qc.txt'),
-                            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.qc.txt'),
-                            'size': 1339}
-                    ],
-                    'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23'),
-                    'path': os.path.join(output_dir, 'facets/Sample24.Sample23')
-                }
+            OFile(name = "Sample24.gene_level.txt", hash = "4e916a52458151007486bf536acfff539fdc2ecc", size = 148195, dir = output_dir_sample24)
             ],
-            'location': 'file://' + os.path.join(output_dir, 'facets'),
-            'path': os.path.join(output_dir, 'facets')
-        },
-        'hisens_seg': [{
-            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.seg'),
-            'basename': 'Sample24_hisens.seg',
-            'class': 'File',
-            'checksum': 'sha1$652f9c6d0245af49bac6ca67a089af7d4e46801b',
-            'size': 1897,
-            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.seg')
-        }],
-        'purity_rds': [{
-            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_purity.rds'),
-            'basename': 'Sample24_purity.rds',
-            'class': 'File',
-            'checksum': 'sha1$dd8b967f84b191ff76214c6110db8d0e65f6514c',
-            'size': 213356,
-            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_purity.rds')
-        }],
-        'purity_seg': [{
-            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_purity.seg'),
-            'basename': 'Sample24_purity.seg',
-            'class': 'File',
-            'checksum': 'sha1$591e6d8b432e1e910fe4fb4b1814508131f960c9',
-            'size': 1285,
-            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_purity.seg')
-        }],
-        'qc_txt': [{
-            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.qc.txt'),
-            'basename': 'Sample24.qc.txt',
-            'class': 'File',
-            'checksum': 'sha1$d4a36726a5fcb7b268aae02d97ce4e382e42d9f6',
-            'size': 1339,
-            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24.qc.txt')
-        }],
-        'hisens_cncf_txt': [{
-            'basename': 'Sample24_hisens.cncf.txt',
-            'checksum': 'sha1$db9131a33889a1cac82e3bd6b3f0e5e182c65105',
-            'class': 'File',
-            'location': 'file://' + os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.cncf.txt'),
-            'path': os.path.join(output_dir, 'facets/Sample24.Sample23/Sample24_hisens.cncf.txt'),
-            'size': 5238
-           }],
+        'hisens_rds': [
+            OFile(name = "Sample24_hisens.rds", hash = "6bfd6c7f29c49ec8ef538dd468a3b4626b05bda2", size = 213986, dir = output_dir_sample24)
+            ],
+        'output_dir': ODir(name = 'facets', dir = output_dir, items = [
+            ODir(name = 'Sample24.Sample23', items = [
+                OFile(name = 'Sample24.Sample23_hisens.ccf.portal.maf', size = 11996607, hash = 'd91a8e15c66429b09f1b7db41bc38bdfa0b84c64'),
+                OFile(name = 'Sample24.arm_level.txt', size = 1824, hash = 'df37c54ae4969257e436a7a7a595c42ef19ecbb5'),
+                OFile(name = 'Sample24.txt', size = 529, hash = 'a0fb3df832efc18a66a8a54e5609666da5f4d7d7'),
+                OFile(name = 'Sample24.gene_level.txt', size = 148195, hash = '4e916a52458151007486bf536acfff539fdc2ecc'),
+                OFile(name = 'Sample24_hisens.cncf.txt', size = 5238, hash = 'db9131a33889a1cac82e3bd6b3f0e5e182c65105'),
+                OFile(name = 'Sample24_hisens.rds', size = 213986, hash = '6bfd6c7f29c49ec8ef538dd468a3b4626b05bda2'),
+                OFile(name = 'Sample24_hisens.seg', size = 1897, hash = '652f9c6d0245af49bac6ca67a089af7d4e46801b'),
+                OFile(name = 'Sample24_purity.rds', size = 213356, hash = 'dd8b967f84b191ff76214c6110db8d0e65f6514c'),
+                OFile(name = 'Sample24_purity.seg', size = 1285, hash = '591e6d8b432e1e910fe4fb4b1814508131f960c9'),
+                OFile(name = 'Sample24.qc.txt', size = 1339, hash = 'd4a36726a5fcb7b268aae02d97ce4e382e42d9f6'),
+                ])
+            ]),
+        'hisens_seg': [
+            OFile(name = "Sample24_hisens.seg", hash = "652f9c6d0245af49bac6ca67a089af7d4e46801b", size = 1897, dir = output_dir_sample24)
+            ],
+        'purity_rds': [
+            OFile(name = "Sample24_purity.rds", hash = "dd8b967f84b191ff76214c6110db8d0e65f6514c", size = 213356, dir = output_dir_sample24)
+            ],
+        'purity_seg': [
+            OFile(name = "Sample24_purity.seg", hash = "591e6d8b432e1e910fe4fb4b1814508131f960c9", size = 1285, dir = output_dir_sample24)
+            ],
+        'qc_txt': [
+            OFile(name = "Sample24.qc.txt", hash = "d4a36726a5fcb7b268aae02d97ce4e382e42d9f6", size = 1339, dir = output_dir_sample24)
+            ],
+        'hisens_cncf_txt': [
+            OFile(name = "Sample24_hisens.cncf.txt", hash = "db9131a33889a1cac82e3bd6b3f0e5e182c65105", size = 5238, dir = output_dir_sample24)
+               ],
         }
 
         self.assertCWLDictEqual(output_json, expected_output)
