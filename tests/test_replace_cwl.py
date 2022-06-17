9a3a56bf2d3be3a6cd88d2c5b3697c58813cf32f
Stephen Kelly <stevekm@users.noreply.github.com>
2022-06-17 14:34:27 -0400

    clean up tests/test_replace_cwl.py

--- tests/test_replace_cwl.py
+++ tests/test_replace_cwl.py
@@ -5,83 +5,50 @@ unit tests for the replace.cwl file
 """
 import os
 import sys
-import json
 import unittest
-from tempfile import TemporaryDirectory
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 PARENT_DIR = os.path.dirname(THIS_DIR)
 sys.path.insert(0, PARENT_DIR)
-from pluto.tools import run_command, CWLFile
-from pluto.settings import CWL_ARGS, FACETS_SNPS_VCF, DATA_SETS
+from pluto.tools import PlutoTestCase, CWLFile
+from pluto.serializer import OFile
 sys.path.pop(0)
 
-cwl_file = CWLFile('replace.cwl')
+class TestReplace(PlutoTestCase):
+    cwl_file = CWLFile('replace.cwl')
 
-class TestReplace(unittest.TestCase):
     def test_replace1(self):
         """
         Test that strings get replaced
         """
-        with TemporaryDirectory() as tmpdir:
-            # make a dummy file with some lines
-            input_lines = ["HEADER", "foo", "ILLOGICAL", "baz"]
-            input_file = os.path.join(tmpdir, "input.txt")
-            with open(input_file, "w") as fout:
-                for line in input_lines:
-                    fout.write(line + '\n')
+        # make a dummy file with some lines
+        input_lines = ["HEADER", "foo", "ILLOGICAL", "baz"]
+        input_file = os.path.join(self.tmpdir, "input.txt")
+        with open(input_file, "w") as fout:
+            for line in input_lines:
+                fout.write(line + '\n')
 
-            input_json = {
-                "input_file":{
-                      "class": "File",
-                      "path": input_file
-                    },
-                }
-            input_json_file = os.path.join(tmpdir, "input.json")
-            with open(input_json_file, "w") as input_json_file_data:
-                json.dump(input_json, input_json_file_data)
+        self.input = {
+            "input_file":{
+                  "class": "File",
+                  "path": input_file
+                },
+            }
 
-            output_dir = os.path.join(tmpdir, "output")
-            tmp_dir = os.path.join(tmpdir, "tmp")
-            cache_dir = os.path.join(tmpdir, "cache")
+        output_json, output_dir = self.run_cwl()
 
-            command = [
-            "cwl-runner",
-            *CWL_ARGS,
-            "--outdir", output_dir,
-            "--tmpdir-prefix", tmp_dir,
-            "--cachedir", cache_dir,
-            cwl_file, input_json_file
-            ]
+        # check the contents of the concatenated file; should be the same as the input
+        output_file = os.path.join(output_dir, 'output.txt')
+        with open(output_file) as fin:
+            output_lines = [ line.strip() for line in fin ]
 
-            returncode, proc_stdout, proc_stderr = run_command(command)
+        expected_lines = ["HEADER", "foo", "NA", "baz"]
+        self.assertEqual(output_lines, expected_lines)
 
-            if returncode != 0:
-                print(proc_stderr)
-
-            self.assertEqual(returncode, 0)
-
-            output_json = json.loads(proc_stdout)
-
-            # check the contents of the concatenated file; should be the same as the input
-            output_file = os.path.join(output_dir, 'output.txt')
-            with open(output_file) as fin:
-                output_lines = [ line.strip() for line in fin ]
-
-            expected_lines = ["HEADER", "foo", "NA", "baz"]
-            self.assertEqual(output_lines, expected_lines)
-
-            expected_output = {
-                'output_file': {
-                    'location': 'file://' + os.path.join(output_dir, 'output.txt'),
-                    'basename': 'output.txt',
-                    'class': 'File',
-                    'checksum': 'sha1$62255c8ee13b8ba6e01c7e17262a8ba1f174e5cb',
-                    'size': 18,
-                    'path': os.path.join(output_dir, 'output.txt')
-                    }
-                }
-            self.assertDictEqual(output_json, expected_output)
+        expected_output = {
+            'output_file': OFile(name = "output.txt", size = 18, hash = "62255c8ee13b8ba6e01c7e17262a8ba1f174e5cb", dir = output_dir)
+            }
+        self.assertCWLDictEqual(output_json, expected_output)
 
 
 if __name__ == "__main__":
