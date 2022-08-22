e4a09acc823d7de0209af188c663ddee9d007084
Stephen Kelly <stevekm@users.noreply.github.com>
2022-08-22 19:58:14 -0400

    add test case and CWL for index_sample_bam.cwl

---
+++ cwl/index_sample_bam.cwl
@@ -0,0 +1,38 @@
+#!/usr/bin/env cwl-runner
+cwlVersion: v1.2
+class: CommandLineTool
+doc: index a Fillout sample .bam file
+baseCommand: [ "bash", "run.sh" ]
+requirements:
+  - $import: types.yml
+  - class: InlineJavascriptRequirement
+  - class: ResourceRequirement
+    coresMin: 4 # make sure this matches what is in the command below!
+  - class: DockerRequirement
+    dockerPull: mskcc/helix_filters_01:samtools-1.9
+  - class: InitialWorkDirRequirement
+    listing:
+      - $(inputs.sample['bam_file']) # NOTE: why are we doing this?? # NOTE: I think this is causing all input bam files to get copied when using Toil??
+      - entryname: run.sh
+        entry: |-
+          set -eu
+          # sample.bam
+          input_bam="$(inputs.sample['bam_file'].basename)"
+          # sample.bam.bai
+          default_bai="\${input_bam}.bai"
+          # sample.bai
+          extra_bai="\${input_bam%.*}.bai"
+          samtools index -@ 4 "\${input_bam}"
+          cp "\${default_bai}" "\${extra_bai}"
+
+inputs:
+  sample: "types.yml#FilloutIndexSample"
+outputs:
+  sample:
+    type: "types.yml#FilloutIndexedSample"
+    outputBinding:
+      outputEval: ${
+        var ret = inputs.sample;
+        ret['bam_file']['secondaryFiles'] = [{"class":"File", "path":runtime.outdir + "/" + inputs.sample["bam_file"].nameroot + ".bai"}];
+        return ret;
+        }
---
+++ tests/test_index_sample_bam_cwl.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Test case for the index_sample_bam cwl
+"""
+import os
+import sys
+import unittest
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+PARENT_DIR = os.path.dirname(THIS_DIR)
+sys.path.insert(0, PARENT_DIR)
+from pluto.tools import PlutoTestCase, CWLFile
+from pluto.settings import DATA_SETS
+from pluto.serializer import OFile
+sys.path.pop(0)
+
+class TestIndexSampleBam(PlutoTestCase):
+    cwl_file = CWLFile('index_sample_bam.cwl')
+
+    def test_index1(self):
+        """
+        Test case for running the fillout workflow on a number of samples, each with a bam and maf
+        """
+        sample1_maf = os.path.join(DATA_SETS['Fillout01']['MAF_DIR'], 'Sample1.FillOutUnitTest01.muts.maf')
+        sample1_bam = os.path.join(DATA_SETS['Fillout01']['BAM_DIR'], 'Sample1.UnitTest01.bam')
+
+        self.input = {
+        "sample": { # "types.yml#FilloutIndexSample"
+            "sample_id": "Sample1",
+            "normal_id": "FROZENPOOLEDNORMAL_IMPACT505_V2",
+            "sample_type": "research",
+            "prefilter": True,
+            "maf_file": { "class": "File", "path": sample1_maf },
+            "bam_file": { "class": "File", "path": sample1_bam }
+            }
+        }
+
+        output_json, output_dir = self.run_cwl()
+
+        expected_output = {
+            'sample': {
+                'bam_file': OFile(name = "Sample1.UnitTest01.bam", hash = "c77293b997de1f12e6ba155cde37eb06210af711", size = 4454012599, dir = output_dir,
+                    secondaryFiles = [OFile(name = "Sample1.UnitTest01.bai", hash = "fc2d6cc4ebeac0fca2b6c5f19ab2a9930cb8f349", size = 4182280, dir = output_dir)]),
+                'maf_file': OFile(name = "Sample1.FillOutUnitTest01.muts.maf", hash = "c49c5ea6e0f755d5229ff761e0cb10bc7df72a25", size = 101253, dir = output_dir),
+                'normal_id': 'FROZENPOOLEDNORMAL_IMPACT505_V2',
+                'prefilter': True,
+                'sample_id': 'Sample1',
+                'sample_type': 'research'
+            },
+        }
+        self.assertCWLDictEqual(output_json, expected_output)
+
+
+if __name__ == "__main__":
+    unittest.main()
