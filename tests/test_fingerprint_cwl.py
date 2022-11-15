b15a9be9efef6964d174f110f8d3f804d14898d4
Stephen Kelly <stevekm@users.noreply.github.com>
2022-11-15 14:55:19 -0500

    update test case for tests/test_fingerprint_cwl.py add line count and header check, remove hash and size check

--- tests/test_fingerprint_cwl.py
+++ tests/test_fingerprint_cwl.py
@@ -6,8 +6,6 @@ unit tests for the fingerprint.cwl
 import os
 import sys
 import unittest
-# import json
-# from tempfile import TemporaryDirectory
 
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 PARENT_DIR = os.path.dirname(THIS_DIR)
@@ -20,13 +18,9 @@ sys.path.pop(0)
 class TestFingerprint(PlutoTestCase):
     cwl_file = CWLFile('fingerprint.cwl')
 
-    
     def test_fingerprint_1(self):
         """
         """
-
-        self.preserve = True
-
         self.input = {
             "conpair_markers_txt": {
                 "path": "/juno/work/ci/concordance-workflow/markers/IMPACT468/FP_tiling_genotypes_for_Conpair.txt",
@@ -40,32 +34,30 @@ class TestFingerprint(PlutoTestCase):
                 "path": "/juno/work/ci/resources/genomes/GRCh37/fasta/b37.fasta",
                 "class": "File"
                 },
-            "runparams": {
-                "tmp_dir": "/scratch",
-                "gatk_jar_path": "/usr/bin/gatk.jar"
-                },
             "tumor_bam": {
                 "path": "/work/ci/dmp_finderprint_matching/dummy_bam/dummy.bam",
                 "class": "File"
                 },
-            "dmp_dir": {
+            "dmp_dir": { ##"/work/ci/dmp/likelihoods"
                 "class": "Directory",
                 "path": "/work/ci/dmp_finderprint_matching/dummy_pickle"
                 }
             }
 
-
-
-##"/work/ci/dmp/likelihoods"
-
         output_json, output_dir = self.run_cwl()
-        
+
         expected_output = {
-            'output_file': OFile(name = 'dummy.concordance.tsv', hash = "sha1$6b1a5ef498be9c15c842033ba7b5cc970397b5f5", size = 487, dir = output_dir)
+            'output_file': OFile(name = 'dummy.concordance.tsv', dir = output_dir)
             }
 
-
-        self.assertCWLDictEqual(expected_output, output_json)
+        # file contenst are inconsistent so strip some keys from the output dict
+        strip_related_keys = [
+            ('basename', 'dummy.concordance.tsv', ['size', 'checksum']),
+        ]
+        self.assertCWLDictEqual(output_json, expected_output, related_keys = strip_related_keys)
+        self.assertNumMutations(os.path.join(output_dir, 'dummy.concordance.tsv'), 2)
+        self.assertHeaderEquals(os.path.join(output_dir, 'dummy.concordance.tsv'),
+            ['concordance', 'num_markers_used', 'num_total_markers', "tumor", "normal", "tumor_pileup", "normal_pileup"])
 
 
 
